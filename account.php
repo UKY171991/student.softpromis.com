a87ce3103949a4cb37ba3d21ab74280ee192d620
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-06 13:59:53 +0530

    Update account.php

--- account.php
+++ account.php
@@ -347,7 +347,7 @@ $monthLabels = array_reverse($monthLabels);
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 
-    <canvas id="barChart"></canvas>
+    <!-- <canvas id="barChart"></canvas> -->
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
 
