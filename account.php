7a91af6e154d3c7c1b77f56fe58286d4a01a754f
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-07 12:21:26 +0530

    Update account.php

--- account.php
+++ account.php
@@ -493,6 +493,7 @@ $monthLabels = array_reverse($monthLabels);
 
   <!-- jQuery (Required for DataTables and Bootstrap) -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></script>
 
     <!-- Bootstrap Bundle (JS + Popper.js) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
