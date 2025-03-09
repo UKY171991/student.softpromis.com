808190e78c9878e878554f69a8da5464804f634c
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-09 11:52:20 +0530

    Update add-candidate.php

--- add-candidate.php
+++ add-candidate.php
@@ -277,7 +277,7 @@ if (strlen($_SESSION['alogin']) == "") {
     <div class="main-wrapper">
 
         <!-- ========== TOP NAVBAR ========== -->
-        <?php include('includes/topbar.php'); ?>
+        <?php // include('includes/topbar.php'); ?>
         <!-- ========== WRAPPER FOR BOTH SIDEBARS & MAIN CONTENT ========== -->
         <div class="content-wrapper">
             <div class="content-container">
