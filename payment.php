56d928ce3987b7865298ebaac569c7198ce1e3c4
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-09 11:38:50 +0530

    Update payment.php

--- payment.php
+++ payment.php
@@ -615,8 +615,8 @@ if (strlen($_SESSION['alogin']) == "") {
                         }
                         .contact-info {
                             text-align: center;
-                            margin-top: 20px;
-                            font-size: 14px;
+                            margin-top: 0px;
+                            font-size: 12px;
                         }
 
                         .table>tbody>tr>td, .table>tbody>tr>th, .table>tfoot>tr>td, .table>tfoot>tr>th, .table>thead>tr>td, .table>thead>tr>th {
