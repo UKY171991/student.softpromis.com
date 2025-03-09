12d7ce8684f0bbbdc74fdc427b65026108304831
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-09 11:41:03 +0530

    Update payment.php

--- payment.php
+++ payment.php
@@ -547,7 +547,7 @@ if (strlen($_SESSION['alogin']) == "") {
                             width: 100%;
                             max-width: 600px;
                             background: #fff;
-                            padding: 20px;
+                            padding: 15px;
                             border: 2px solid #000;
                             border-radius: 10px;
                             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
