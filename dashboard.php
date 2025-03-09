6d6657d942ee180ad17acd26970cd9c88226de22
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-09 12:50:25 +0530

    h

--- dashboard.php
+++ dashboard.php
@@ -7,7 +7,7 @@ if (strlen($_SESSION['alogin']) == "") {
 } else {
 ?>
 <!DOCTYPE html> 
-<html lang="en">
+<html lang="en"> 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
