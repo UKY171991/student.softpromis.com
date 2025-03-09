8b01b177383a32e1d54b882135242cf3086014c0
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-09 11:58:31 +0530

    Update add-candidate.php

--- add-candidate.php
+++ add-candidate.php
@@ -223,6 +223,7 @@ if (strlen($_SESSION['alogin']) == "") {
 <body class="top-navbar-fixed">
 
     <?php include('includes/topbar-new.php'); ?>
+    <?php include('includes/left-sidebar-new.php'); ?>
 
     <div class="main-wrapper">
 
@@ -234,7 +235,6 @@ if (strlen($_SESSION['alogin']) == "") {
 
                 <!-- ========== LEFT SIDEBAR ========== -->
                 <?php // include('includes/leftbar.php'); ?>
-                <?php include('includes/left-sidebar-new.php'); ?>
                 <!-- /.left-sidebar -->
 
                 <div class="main-page">
