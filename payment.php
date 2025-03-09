188e78dd62f37cdbb088d4b1cffa9062cfcac7fd
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-09 10:44:00 +0530

    Update payment.php

--- payment.php
+++ payment.php
@@ -543,10 +543,6 @@ if (strlen($_SESSION['alogin']) == "") {
                     <title>Payment Receipt</title>
                     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
                     <style>
-                        /*body {
-                            background-color: #f8f9fa;
-                            font-family: Arial, sans-serif;
-                        }*/
                         .receipt {
                             width: 100%;
                             max-width: 600px;
