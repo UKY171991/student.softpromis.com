b73eceea6054826b04c3a003266181b98b99c704
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-09 07:29:59 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -389,7 +389,6 @@ if (strlen($_SESSION['alogin']) == "") {
   <script src="js/jquery/jquery-2.2.4.min.js"></script>
   <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="js/bootstrap/bootstrap.min.js"></script>
   <script src="js/pace/pace.min.js"></script>
   <script src="js/lobipanel/lobipanel.min.js"></script>
   <script src="js/iscroll/iscroll.js"></script>
