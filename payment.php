8c2b9d79e01083be07b0911bf9661d6d649b40c0
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-09 10:32:19 +0530

    Update payment.php

--- payment.php
+++ payment.php
@@ -556,7 +556,7 @@ if (strlen($_SESSION['alogin']) == "") {
                             border-radius: 10px;
                             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
                             margin: 30px auto;
-                            height: fit-content;
+                            height: auto;
                         }
                         .receipt-header {
                             text-align: center;
