1d957f7ddc008986b6db411cd522953b9273494a
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-09 07:31:17 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -384,7 +384,7 @@ if (strlen($_SESSION['alogin']) == "") {
   </div><!-- /.container-fluid -->
 
   <!-- Bootstrap Bundle with Popper -->
-  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
   <script src="js/jquery/jquery-2.2.4.min.js"></script>
   <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
