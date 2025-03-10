4f43397d8ce15f0d67d72988ecdc283a03a61dea
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-10 15:38:50 +0530

    Update edit-candidate.php

--- edit-candidate.php
+++ edit-candidate.php
@@ -128,6 +128,7 @@ if (strlen($_SESSION['alogin']) == "") {
     <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen">
     <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen">
     <link rel="stylesheet" href="css/prism/prism.css" media="screen"> <!-- USED FOR DEMO HELP - YOU CAN REMOVE IT -->
+    <link rel="stylesheet" href="css/select2/select2.min.css">
     <link rel="stylesheet" href="css/main.css" media="screen">
     <script src="js/modernizr/modernizr.min.js"></script>
 </head>
@@ -518,6 +519,7 @@ if (strlen($_SESSION['alogin']) == "") {
     <script src="js/pace/pace.min.js"></script>
     <script src="js/lobipanel/lobipanel.min.js"></script>
     <script src="js/iscroll/iscroll.js"></script>
+    <script src="js/select2/select2.min.js"></script>
 
     <!-- ========== PAGE JS FILES ========== -->
     <script src="js/prism/prism.js"></script>
