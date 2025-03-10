fff12014dc0d8d4b49e24c7f646ea36aa9d6c472
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-10 16:02:13 +0530

    Update get_batches.php

--- get_batches.php
+++ get_batches.php
@@ -2,7 +2,7 @@
 include('includes/config.php');
 
 if (isset($_POST['job_id'])) {
-    $job_id = intval($_POST['job_id']);
+    echo $job_id = intval($_POST['job_id']);
 
     // Example Query: Adjust according to your actual database schema
     $sql = "SELECT id,job_role_id, batch_name FROM tblbatch WHERE job_role_id = :job_id ORDER BY id DESC";
