66fa85a653e946a0ec6a860ab71d8d86e357981d
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-10 17:07:01 +0530

    Update get_batches.php

--- get_batches.php
+++ get_batches.php
@@ -29,23 +29,24 @@ if (isset($_POST['training_center'])) {
     $sch_s = $query_s->fetchAll(PDO::FETCH_ASSOC);
 
     foreach ($sch_s as $row5) {
-    	echo $row5['id'];
+    	$scheme_id = $row5['scheme_id'];
+
+    	// Example Query: Adjust according to your actual database schema
+	    $sql = "SELECT SchemeId, SchemeName FROM tblscheme WHERE scheme_id = :scheme_id ORDER BY id DESC";
+	    $query = $dbh->prepare($sql);
+	    $query->bindParam(':scheme_id', $scheme_id, PDO::PARAM_INT);
+	    $query->execute();
+
+	    $training_center = $query->fetchAll(PDO::FETCH_ASSOC);
+
+	    //print_r($training_center);
+
+	    echo json_encode($training_center);
+
     }
 
     //print_r($sch_s[0]['scheme_id']);
 
     exit();
-
-    // Example Query: Adjust according to your actual database schema
-    $sql = "SELECT SchemeId, SchemeName FROM tblscheme WHERE job_roll_id = :job_id ORDER BY id DESC";
-    $query = $dbh->prepare($sql);
-    $query->bindParam(':job_id', $job_id, PDO::PARAM_INT);
-    $query->execute();
-
-    $training_center = $query->fetchAll(PDO::FETCH_ASSOC);
-
-    //print_r($training_center);
-
-    echo json_encode($training_center);
 }
 ?>
