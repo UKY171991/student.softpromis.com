572e6d7cbffd85cb52efe253c90cffad39fb42aa
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-10 15:34:40 +0530

    Update add-candidate.php

--- add-candidate.php
+++ add-candidate.php
@@ -707,12 +707,14 @@ $(document).ready(function(){
         $(this).val(value);
     });
 });
-</script>
 
 // In your Javascript (external .js resource or <script> tag)
 $(document).ready(function() {
     $('.js-example-basic-single').select2();
 });
+</script>
+
+
   
 </body>
 
