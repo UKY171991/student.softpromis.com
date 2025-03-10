3e742aff522881369cf99978c4c43b06986d3c33
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-10 15:59:23 +0530

    Update get_batches.php

--- get_batches.php
+++ get_batches.php
@@ -5,15 +5,12 @@ if (isset($_POST['job_id'])) {
     $job_id = intval($_POST['job_id']);
 
     // Example Query: Adjust according to your actual database schema
-    $sql = "SELECT id, batch_name FROM tblbatch WHERE id = :job_id ORDER BY id DESC";
+    $sql = "SELECT id, batch_name FROM tblbatch WHERE job_role_id = :job_id ORDER BY id DESC";
     $query = $dbh->prepare($sql);
     $query->bindParam(':job_id', $job_id, PDO::PARAM_INT);
-    //$query->bindParam(':batch_name', $batch_name, PDO::PARAM_STR);
     $query->execute();
 
     $batches = $query->fetchAll(PDO::FETCH_ASSOC);
-
-    print_r($batches);
 
     echo json_encode($batches);
 }
