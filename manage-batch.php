5a435c678474737a3bdcb54aa8b2edc1903ae502
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-11 15:03:59 +0530

    Update manage-batch.php

--- manage-batch.php
+++ manage-batch.php
@@ -130,8 +130,8 @@ if (strlen($_SESSION['alogin']) == "") {
                                                 <tbody>
                                                     <?php 
                                                     //$sql = "SELECT tbltrainingcenter.trainingcentername,tblscheme.SchemeName,tblsector.SectorName,tbljobroll.jobrollname,tblbatch.* from tbltrainingcenter,tblscheme,tblsector,tbljobroll,tblbatch WHERE tblbatch.training_centre_id  = tbltrainingcenter.TrainingcenterId AND tblbatch.scheme_id = tblscheme.SchemeId AND tblbatch.sector_id = tblsector.SectorId AND tblbatch.job_roll_id = tbljobroll.JobrollId";
-                                                    
-                                                    $sql = "SELECT tbltrainingcenter.trainingcentername,tblscheme.SchemeName,tblsector.SectorName,tbljobroll.jobrollname,tblbatch.* from tbltrainingcenter,tblscheme,tblsector,tbljobroll,tblbatch;";
+
+                                                    $sql = "SELECT tbltrainingcenter.trainingcentername,tblscheme.SchemeName,tblsector.SectorName,tbljobroll.jobrollname,tblbatch.* from tbltrainingcenter,tblscheme,tblsector,tbljobroll,tblbatch WHERE tblbatch.training_centre_id  = tbltrainingcenter.TrainingcenterId";
                                                         $query = $dbh->prepare($sql);
                                                         $query->execute();
                                                         $results = $query->fetchAll(PDO::FETCH_OBJ);
