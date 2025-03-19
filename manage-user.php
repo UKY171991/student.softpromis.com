6751a5b4099ad0a4b52d1b29d2f8a2f96fa034d5
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-19 14:38:57 +0530

    Update manage-user.php

--- manage-user.php
+++ manage-user.php
@@ -26,14 +26,14 @@ if (strlen($_SESSION['alogin']) == "") {
     <link rel="stylesheet" href="css/prism/prism.css" media="screen">
     <link rel="stylesheet" href="css/select2/select2.min.css">
     <link rel="stylesheet" href="css/main.css" media="screen">
-<?php /*
+
     <link rel="stylesheet" type="text/css" href="js/DataTables/datatables.min.css" />
 
     <link rel="stylesheet" href="https://adminlte.io/themes/v3/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
       <link rel="stylesheet" href="https://adminlte.io/themes/v3/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
       <link rel="stylesheet" href="https://adminlte.io/themes/v3/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
       
-
+<?php /*
     <link rel="stylesheet" href="css/mystyle.css"> 
     <script src="js/modernizr/modernizr.min.js"></script>
 
