dc1c891391f2e0eb831555cffbe11069a838cba5
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-19 14:14:59 +0530

    Update manage-user.php

--- manage-user.php
+++ manage-user.php
@@ -287,3 +287,190 @@ $(document).ready(function() {
 
 });
 </script>
+
+
+
+
+
+
+
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if (strlen($_SESSION['alogin']) == "") {
+    header("Location: index.php");
+} else {
+?>
+<!DOCTYPE html> 
+<html lang="en"> 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SOFTPRO | ADMIN | Dashboard</title>
+
+  <!-- <link rel="stylesheet" href="css/bootstrap.min.css" media="screen"> -->
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
+    <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen">
+    <link rel="stylesheet" href="css/prism/prism.css" media="screen">
+    <link rel="stylesheet" href="css/select2/select2.min.css">
+    <link rel="stylesheet" href="css/main.css" media="screen">
+    <link rel="stylesheet" href="css/mystyle.css"> 
+    <script src="js/modernizr/modernizr.min.js"></script>
+
+  <!-- Bootstrap 5 CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Font Awesome 6 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <link rel="stylesheet" href="includes/style.css">
+
+</head>
+<body>
+  <!-- Top Navbar -->
+  <?php include('includes/topbar-new.php'); ?>
+  
+  <div class="container-fluid">
+    <div class="row">
+      <!-- Sidebar -->
+
+      <?php include('includes/left-sidebar-new.php'); ?>
+       <?php include('includes/leftbar.php'); ?>
+
+
+      <!-- Main Content -->
+      <main class="col-lg-10 col-md-9 p-4">
+        <h2 class="mb-4">Softpro Dashboard</h2>
+        <div class="row g-3">
+            <!-- Regd Candidates Card -->
+           <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-12">
+
+                                    <div class="panel">
+                                        <div class="panel-heading">
+                                            <div class="panel-title">
+                                                <h5>MAnage user</h5>
+                                            </div>
+                                        </div>
+                                        <?php if ($msg) { ?>
+                                        <div class="alert alert-success left-icon-alert" role="alert">
+                                            <strong>Well done!</strong>
+                                            <?php echo htmlentities($msg); ?>
+                                        </div>
+                                        <?php } else if ($error) { ?>
+                                        <div class="alert alert-danger left-icon-alert" role="alert">
+                                            <strong>Oh snap!</strong>
+                                            <?php echo htmlentities($error); ?>
+                                        </div>
+                                        <?php } ?>
+                                        <div class="panel-body p-20" style="overflow: scroll;">
+                                            <table id="example"
+                                                class="table table-stripped table-bordered table-hover table-full-width table-grey table-responsive-lg"
+                                                cellspacing="0" width="100%">
+                                                <thead>
+                                                    <tr>
+                                                        <th>#</th>
+                                                        <th>User Name</th>
+                                                        <th>Email</th>
+                                                        <th>User Type</th>
+                                                        <th>Updated Date</th>
+                                                        <th>Action</th>
+                                                    </tr>
+                                                </thead>
+                                                <tfoot>
+                                                    <tr>
+                                                        <th>#</th>
+                                                        <th>User Name</th>
+                                                        <th>Email</th>
+                                                        <th>User Type</th>
+                                                        <th>Updated Date</th>
+                                                        <th>Action</th>
+                                                    </tr>
+                                                </tfoot>
+                                                <tbody>
+                                                    <?php $sql = "SELECT * from admin";
+                                                        $query = $dbh->prepare($sql);
+                                                        $query->execute();
+                                                        $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                                        $cnt = 1;
+                                                        if ($query->rowCount() > 0) {
+                                                            foreach ($results as $result) {   ?>
+                                                    <tr>
+                                                        <td>
+                                                            <?php echo htmlentities($cnt); ?>
+                                                        </td>
+                                                        <td>
+                                                            <?php echo htmlentities($result->UserName); ?>
+                                                        </td>
+                                                        <td>
+                                                            <?php echo htmlentities($result->email); ?>
+                                                        </td>
+                                                        <td>
+                                                            <?php 
+                                                                if($result->user_type ==1 OR $result->user_type ==0){ 
+                                                                    echo "Admin";
+                                                                }elseif($result->user_type ==2){
+                                                                    echo "MIS";
+                                                                }elseif($result->user_type ==3){
+                                                                    echo "Training Center";
+                                                                }
+                                                             ?>
+                                                        </td>
+                                                        <td>
+                                                            <?php echo htmlentities($result->updationDate); ?>
+                                                        </td>
+                                                        <td>
+                                                            <a class="badge badge-info"
+                                                                href="edit-user.php?userid=<?php echo htmlentities($result->id); ?>"><i
+                                                                    class="fa fa-edit" title="Edit Record"></i> </a>
+
+                                                            <a class="badge badge-danger delete"
+                                                                id='del_<?php echo htmlentities($result->id); ?>'><i
+                                                                    class="txt txt-danger fa fa-trash"
+                                                                    title="delete Record"></i> </a>
+
+                                                        </td>
+                                                    </tr>
+                                                    <?php $cnt = $cnt + 1;
+                                                            }
+                                                        } ?>
+
+
+                                                </tbody>
+                                            </table>
+
+
+                                            <!-- /.col-md-12 -->
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- /.col-md-6 -->
+
+
+                            </div>
+                            <!-- /.col-md-12 -->
+                        </div>
+           
+        </div><!-- /.row -->
+    </main>
+    </div><!-- /.row -->
+  </div><!-- /.container-fluid -->
+
+  <!-- Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+  <script src="js/jquery/jquery-2.2.4.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
+  <script src="js/bootstrap/bootstrap.min.js"></script>
+  <script src="js/pace/pace.min.js"></script>
+  <script src="js/lobipanel/lobipanel.min.js"></script>
+  <script src="js/iscroll/iscroll.js"></script>
+  <script src="js/prism/prism.js"></script>
+  <script src="js/select2/select2.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
+<?php } ?>
