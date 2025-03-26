41dd5c587946061df0fcff7824148d7135ce19bd
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-26 11:51:53 +0530

    Update pending_payment_approval.php

--- pending_payment_approval.php
+++ pending_payment_approval.php
@@ -5,14 +5,9 @@ include('includes/config.php');
 if (strlen($_SESSION['alogin']) == "") {
     header("Location: index.php");
 } else {
-
-    // if($_SESSION['user_type']!=1){
-    //     header("Location: index.php");
-    // }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,7 +17,7 @@ if (strlen($_SESSION['alogin']) == "") {
     <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
     <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen">
     <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen">
-    <link rel="stylesheet" href="css/prism/prism.css" media="screen"> <!-- USED FOR DEMO HELP - YOU CAN REMOVE IT -->
+    <link rel="stylesheet" href="css/prism/prism.css" media="screen">
     <link rel="stylesheet" type="text/css" href="js/DataTables/datatables.min.css" />
     <link rel="stylesheet" href="css/main.css" media="screen">
     <script src="js/modernizr/modernizr.min.js"></script>
@@ -35,7 +30,6 @@ if (strlen($_SESSION['alogin']) == "") {
         -webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
         box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
     }
-
     .succWrap {
         padding: 10px;
         margin: 0 0 20px 0;
@@ -46,28 +40,21 @@ if (strlen($_SESSION['alogin']) == "") {
     }
     </style>
 </head>
-
 <body class="top-navbar-fixed">
     <div class="main-wrapper">
-
         <!-- ========== TOP NAVBAR ========== -->
         <?php include('includes/topbar.php'); ?>
         <!-- ========== WRAPPER FOR BOTH SIDEBARS & MAIN CONTENT ========== -->
         <div class="content-wrapper">
             <div class="content-container">
                 <?php include('includes/leftbar.php'); ?>
-
                 <div class="main-page">
                     <div class="container-fluid">
                         <div class="row page-title-div">
                             <div class="col-md-6">
                                 <h2 class="title">Manage user</h2>
-
                             </div>
-
-                            <!-- /.col-md-6 text-right -->
                         </div>
-                        <!-- /.row -->
                         <div class="row breadcrumb-div">
                             <div class="col-md-6">
                                 <ul class="breadcrumb">
@@ -76,17 +63,12 @@ if (strlen($_SESSION['alogin']) == "") {
                                     <li class="active">Manage user</li>
                                 </ul>
                             </div>
-
                         </div>
-                        <!-- /.row -->
                     </div>
-                    <!-- /.container-fluid -->
-
                     <section class="section">
                         <div class="container-fluid">
                             <div class="row">
                                 <div class="col-md-12">
-
                                     <div class="panel">
                                         <div class="panel-heading">
                                             <div class="panel-title" style="display: flex;">
@@ -94,12 +76,8 @@ if (strlen($_SESSION['alogin']) == "") {
                                                 $sqls = "SELECT SUM(paid) as total_paid FROM emi_list WHERE added_type = 2";
                                                 $querys = $dbh->prepare($sqls);
                                                 $querys->execute();
-                                                $res = $querys->fetch(PDO::FETCH_OBJ); 
-
-                                                //print_r($res->total_paid);
+                                                $res = $querys->fetch(PDO::FETCH_OBJ);
                                                 ?>
-
-                                                <!-- <h5>Pending Payment Approval : <?=$res->total_paid?></h5> -->
                                                 <h5>Total Pending Amount : <?=$res->total_paid?></h5>
                                             </div>
                                         </div>
@@ -115,9 +93,7 @@ if (strlen($_SESSION['alogin']) == "") {
                                         </div>
                                         <?php } ?>
                                         <div class="panel-body p-20" style="overflow: scroll;">
-                                            <table id="example"
-                                                class="table table-stripped table-bordered table-hover table-full-width table-grey table-responsive-lg"
-                                                cellspacing="0" width="100%">
+                                            <table id="example" class="table table-stripped table-bordered table-hover table-full-width table-grey table-responsive-lg" cellspacing="0" width="100%">
                                                 <thead>
                                                     <tr>
                                                         <th>#</th>
@@ -146,162 +122,93 @@ if (strlen($_SESSION['alogin']) == "") {
                                                 </tfoot>
                                                 <tbody>
                                                     <?php
-                                                     $sql = "SELECT * from emi_list where added_type= 2";
-                                                        $query = $dbh->prepare($sql);
-                                                        $query->execute();
-                                                        $results = $query->fetchAll(PDO::FETCH_OBJ);
-                                                        $cnt = 1;
+                                                    $sql = "SELECT 
+                                                                e.id,
+                                                                e.candidate_id,
+                                                                e.paid,
+                                                                e.created,
+                                                                c.enrollmentid,
+                                                                c.candidatename,
+                                                                p.total_fee,
+                                                                p.paid AS paid_total,
+                                                                p.balance
+                                                            FROM emi_list e
+                                                            INNER JOIN tblcandidate c ON e.candidate_id = c.CandidateId
+                                                            INNER JOIN payment p ON p.added_type = e.added_type
+                                                            WHERE e.added_type = 2";
+                                                    $query = $dbh->prepare($sql);
+                                                    $query->execute();
+                                                    $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                                    $cnt = 1;
 
-
-
-                                                        if ($query->rowCount() > 0) {
-                                                            foreach ($results as $result) { 
-                                                                $candidate_id = $result->candidate_id;
-
-
-                                                                 $sql_p = "SELECT * from payment where added_type= 2";
-                                                                 $query_p = $dbh->prepare($sql_p);
-                                                                 $query_p->execute();
-                                                                 $results_p = $query_p->fetchAll(PDO::FETCH_OBJ);
-
-                                                            
-
-                                                                $sql_c = "SELECT * from tblcandidate where CandidateId= '$candidate_id'";
-                                                                $query_c = $dbh->prepare($sql_c);
-                                                                $query_c->execute();
-                                                                $results_c = $query_c->fetchAll(PDO::FETCH_OBJ);
-
-                                                        ?>
+                                                    if ($query->rowCount() > 0) {
+                                                        foreach ($results as $result) { ?>
                                                     <tr>
-                                                        <td>
-                                                            <?php echo htmlentities($cnt); ?>
-                                                        </td>
-                                                        <td>
-                                                            <?php echo htmlentities($results_c[0]->enrollmentid); ?>
-                                                        </td>
-                                                        <td>
-                                                            <?php echo htmlentities($results_c[0]->candidatename); ?>
-                                                        </td>
-                                                        <td>
-                                                            <?php echo htmlentities($results_p[0]->total_fee); ?>
-                                                        </td>
-
-                                                        <td>
-                                                            <?php echo htmlentities($results_p[0]->paid); ?>
-                                                        </td>
-                                                        <td>
-                                                            <?php echo htmlentities($results_p[0]->balance); ?>
-                                                        </td>
-
-                                                        <td>
-                                                            <?php echo htmlentities($result->paid); ?>
-                                                        </td>
-                                                        
-                                                        <td>
-                                                            <?php echo date("d M Y h:i:s A", strtotime($result->created)); ?>
-                                                        </td>
+                                                        <td><?php echo htmlentities($cnt); ?></td>
+                                                        <td><?php echo htmlentities($result->enrollmentid); ?></td>
+                                                        <td><?php echo htmlentities($result->candidatename); ?></td>
+                                                        <td><?php echo htmlentities($result->total_fee); ?></td>
+                                                        <td><?php echo htmlentities($result->paid_total); ?></td>
+                                                        <td><?php echo htmlentities($result->balance); ?></td>
+                                                        <td><?php echo htmlentities($result->paid); ?></td>
+                                                        <td><?php echo date("d M Y h:i:s A", strtotime($result->created)); ?></td>
                                                         <td>
                                                             <?php
-                                                             if($_SESSION['user_type']!=1){
+                                                            if ($_SESSION['user_type'] != 1) {
                                                                 echo "Pending Approval";
-                                                             }else{
-                                                                echo '<a class="badge badge-info" href="#" onclick="updateStatus( '. htmlentities($result->candidate_id) .', '. htmlentities($result->id) .')">Approve</a>';
-                                                             } 
-                                                             ?>
-                                                            
+                                                            } else {
+                                                                echo '<a class="badge badge-info" href="#" onclick="updateStatus(' . htmlentities($result->candidate_id) . ', ' . htmlentities($result->id) . ')">Approve</a>';
+                                                            }
+                                                            ?>
                                                         </td>
                                                     </tr>
-                                                    <?php $cnt = $cnt + 1;
-                                                            }
-                                                        } ?>
-
-
+                                                    <?php $cnt++;
+                                                        }
+                                                    } ?>
                                                 </tbody>
                                             </table>
-
-
-                                            <!-- /.col-md-12 -->
                                         </div>
                                     </div>
                                 </div>
-                                <!-- /.col-md-6 -->
-
-
                             </div>
-                            <!-- /.col-md-12 -->
                         </div>
+                    </section>
                 </div>
-                <!-- /.panel -->
             </div>
-            <!-- /.col-md-6 -->
-
         </div>
-        <!-- /.row -->
-
     </div>
-    <!-- /.container-fluid -->
-    </section>
-    <!-- /.section -->
-
-    </div>
-    <!-- /.main-page -->
-
-
-
-    </div>
-    <!-- /.content-container -->
-    </div>
-    <!-- /.content-wrapper -->
-
-    </div>
-    <!-- /.main-wrapper -->
-
     <!-- ========== COMMON JS FILES ========== -->
     <script src="js/jquery/jquery-2.2.4.min.js"></script>
     <script src="js/bootstrap/bootstrap.min.js"></script>
     <script src="js/pace/pace.min.js"></script>
     <script src="js/lobipanel/lobipanel.min.js"></script>
     <script src="js/iscroll/iscroll.js"></script>
-
     <!-- ========== PAGE JS FILES ========== -->
     <script src="js/prism/prism.js"></script>
     <script src="js/DataTables/datatables.min.js"></script>
-
     <!-- ========== THEME JS ========== -->
     <script src="js/main.js"></script>
     <script>
     $(function($) {
         $('#example').DataTable();
-
-        $('#example2').DataTable({
-            "scrollY": "300px",
-            "scrollCollapse": true,
-            "paging": false
-        });
-
-        $('#example3').DataTable();
     });
     </script>
 </body>
-
 </html>
 <?php } ?>
 
 <script>
-
-function updateStatus(candidateId,id) {
-    // Ask for confirmation
+function updateStatus(candidateId, id) {
     if (confirm("Are you sure you want to update the status?")) {
         $.ajax({
-            url: 'appve_ajax.php',  // Path to the PHP backend file
+            url: 'appve_ajax.php',
             type: 'POST',
-            data: { candidate_id: candidateId,id:id },
+            data: { candidate_id: candidateId, id: id },
             dataType: 'json',
             success: function(response) {
-                if(response.status === 'success'){
+                if (response.status === 'success') {
                     alert('Status updated successfully!');
-                     window.location.reload();
-                    // Optionally, update the UI here (e.g., change a label or reload a section)
+                    window.location.reload();
                 } else {
                     alert('Update failed: ' + response.message);
                 }
@@ -313,6 +220,4 @@ function updateStatus(candidateId,id) {
         });
     }
 }
-
-
 </script>
