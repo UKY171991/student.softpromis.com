217d05758aa6ebf6c4bfe754b32daff1d745b963
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-26 12:11:04 +0530

    Update pending_payment_approval.php

--- pending_payment_approval.php
+++ pending_payment_approval.php
@@ -35,6 +35,12 @@ if (strlen($_SESSION['alogin']) == "") {
   <!-- Font Awesome 6 -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="includes/style.css">
+  <style type="text/css">
+      select.input-sm {
+            height: 30px;
+            line-height: 20px;
+        }
+  </style>
 
 </head>
 <body>
