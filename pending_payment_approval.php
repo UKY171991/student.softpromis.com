759f1d175034f134af2d0f764be4e78d8aea9a48
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-26 11:45:48 +0530

    Update pending_payment_approval.php

--- pending_payment_approval.php
+++ pending_payment_approval.php
@@ -4,48 +4,60 @@ error_reporting(0);
 include('includes/config.php');
 if (strlen($_SESSION['alogin']) == "") {
     header("Location: index.php");
-} else { 
-?>
-<!DOCTYPE html> 
-<html lang="en"> 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>SOFTPRO | ADMIN | Dashboard</title>
+} else {
 
-  <!-- <link rel="stylesheet" href="css/bootstrap.min.css" media="screen"> -->
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    // if($_SESSION['user_type']!=1){
+    //     header("Location: index.php");
+    // }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SOFTPRO | ADMIN</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
     <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
     <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen">
-    <link rel="stylesheet" href="css/prism/prism.css" media="screen">
-    <link rel="stylesheet" href="css/select2/select2.min.css">
+    <link rel="stylesheet" href="css/prism/prism.css" media="screen"> <!-- USED FOR DEMO HELP - YOU CAN REMOVE IT -->
+    <link rel="stylesheet" type="text/css" href="js/DataTables/datatables.min.css" />
     <link rel="stylesheet" href="css/main.css" media="screen">
-    <link rel="stylesheet" href="css/mystyle.css"> 
     <script src="js/modernizr/modernizr.min.js"></script>
+    <style>
+    .errorWrap {
+        padding: 10px;
+        margin: 0 0 20px 0;
+        background: #fff;
+        border-left: 4px solid #dd3d36;
+        -webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
+        box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
+    }
 
-  <!-- Bootstrap 5 CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Font Awesome 6 -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-  <link rel="stylesheet" href="includes/style.css">
-
+    .succWrap {
+        padding: 10px;
+        margin: 0 0 20px 0;
+        background: #fff;
+        border-left: 4px solid #5cb85c;
+        -webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
+        box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
+    }
+    </style>
 </head>
-<body>
-  <!-- Top Navbar -->
-  <?php include('includes/topbar-new.php'); ?>
-  
-  <div class="container-fluid">
-    <div class="row">
-      <!-- Sidebar -->
 
-      <?php include('includes/left-sidebar-new.php'); ?>
-       <?php include('includes/leftbar.php'); ?>
+<body class="top-navbar-fixed">
+    <div class="main-wrapper">
 
+        <!-- ========== TOP NAVBAR ========== -->
+        <?php include('includes/topbar.php'); ?>
+        <!-- ========== WRAPPER FOR BOTH SIDEBARS & MAIN CONTENT ========== -->
+        <div class="content-wrapper">
+            <div class="content-container">
+                <?php include('includes/leftbar.php'); ?>
 
-
-
+                <div class="main-page">
                     <div class="container-fluid">
                         <div class="row page-title-div">
                             <div class="col-md-6">
@@ -218,23 +230,46 @@ if (strlen($_SESSION['alogin']) == "") {
                             </div>
                             <!-- /.col-md-12 -->
                         </div>
+                </div>
+                <!-- /.panel -->
+            </div>
+            <!-- /.col-md-6 -->
 
-    </div><!-- /.row -->
-  </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.row -->
 
-  <!-- Bootstrap Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    </div>
+    <!-- /.container-fluid -->
+    </section>
+    <!-- /.section -->
 
-  <script src="js/jquery/jquery-2.2.4.min.js"></script>
-  <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
-  <script src="js/bootstrap/bootstrap.min.js"></script>
-  <script src="js/pace/pace.min.js"></script>
-  <script src="js/lobipanel/lobipanel.min.js"></script>
-  <script src="js/iscroll/iscroll.js"></script>
-  <script src="js/prism/prism.js"></script>
-  <script src="js/select2/select2.min.js"></script>
-  <script src="js/main.js"></script>
-  <script>
+    </div>
+    <!-- /.main-page -->
+
+
+
+    </div>
+    <!-- /.content-container -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    </div>
+    <!-- /.main-wrapper -->
+
+    <!-- ========== COMMON JS FILES ========== -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/pace/pace.min.js"></script>
+    <script src="js/lobipanel/lobipanel.min.js"></script>
+    <script src="js/iscroll/iscroll.js"></script>
+
+    <!-- ========== PAGE JS FILES ========== -->
+    <script src="js/prism/prism.js"></script>
+    <script src="js/DataTables/datatables.min.js"></script>
+
+    <!-- ========== THEME JS ========== -->
+    <script src="js/main.js"></script>
+    <script>
     $(function($) {
         $('#example').DataTable();
 
@@ -248,6 +283,7 @@ if (strlen($_SESSION['alogin']) == "") {
     });
     </script>
 </body>
+
 </html>
 <?php } ?>
 
