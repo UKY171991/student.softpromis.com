11a0bfd1ced2106d73e38846f7485f64179f01c2
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-27 17:55:04 +0530

    Update add-candidate-to-particular-batch-new.php

--- add-candidate-to-particular-batch-new.php
+++ add-candidate-to-particular-batch-new.php
@@ -139,133 +139,7 @@ if (strlen($_SESSION['alogin']) == "") {
                             <h5 class="mb-0">Add candidate to particular batch</h5>
                         </div>
                         <div class="card-body">
-                            <form method="post">
-                                
-
-                              
-
-                                <button type="submit" name="submit" class="btn btn-primary">
-                                    <i class="fas fa-check me-2"></i>Add Batch
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                </main>
-            </div>
-        </div>
-    </div>
-
-    <!-- Scripts -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-      <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
-  <script src="js/pace/pace.min.js"></script>
-  <script src="js/lobipanel/lobipanel.min.js"></script>
-  <script src="js/iscroll/iscroll.js"></script>
-  <script src="js/prism/prism.js"></script>
-  <script src="js/DataTables/datatables.min.js"></script>
-  <script src="js/main.js"></script>
-
-
-    
-
-    <script>
-    $(function($) {
-        $('#example').DataTable();
-
-        $('#example2').DataTable({
-            "scrollY": "300px",
-            "scrollCollapse": true,
-            "paging": false
-        });
-
-        $('#example3').DataTable();
-    });
-    </script>
-
-
-   
-</body>
-</html>
-
-
-
-
-
-
-
-
-<!DOCTYPE html> 
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SOFTPRO | ADMIN</title>
-    
-    <style>
-    .errorWrap {
-        padding: 10px;
-        margin: 0 0 20px 0;
-        background: #fff;
-        border-left: 4px solid #dd3d36;
-        -webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-        box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-    }
-
-    .succWrap {
-        padding: 10px;
-        margin: 0 0 20px 0;
-        background: #fff;
-        border-left: 4px solid #5cb85c;
-        -webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-        box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-    }
-    </style>
-</head>
-
-<body class="top-navbar-fixed">
-    <div class="main-wrapper">
-
-        <!-- ========== TOP NAVBAR ========== -->
-        <?php include('includes/topbar.php'); ?>
-        <!-- ========== WRAPPER FOR BOTH SIDEBARS & MAIN CONTENT ========== -->
-        <div class="content-wrapper">
-            <div class="content-container">
-                <?php include('includes/leftbar.php'); ?>
-
-                <div class="main-page">
-                    <div class="container-fluid">
-                        <div class="row page-title-div">
-                            <div class="col-md-6">
-                                <h2 class="title">Add candidate to particular batch</h2>
-                            </div>
-
-                            <!-- /.col-md-6 text-right -->
-                        </div>
-                        <!-- /.row -->
-                        <div class="row breadcrumb-div">
-                            <div class="col-md-6">
-                                <ul class="breadcrumb">
-                                    <li><a href="dashboard.php"><i class="fa fa-home"></i> Home</a></li>
-                                    <li> Training Center</li>
-                                    <li class="active">Add candidate to particular batch</li>
-                                </ul>
-                            </div>
-
-                        </div>
-                        <!-- /.row -->
-                    </div>
-                    <!-- /.container-fluid -->
-
-                    <section class="section">
-                        <div class="container-fluid">
-
-
-
+                            
                             <div class="row">
                                 <div class="col-md-12">
 
@@ -387,35 +261,45 @@ if (strlen($_SESSION['alogin']) == "") {
                                 </div>
                                 <!-- /.col-md-12 -->
                             </div>
+
                         </div>
-                        <!-- /.panel -->
-                </div>
-                <!-- /.col-md-6 -->
-
+                    </div>
+                </main>
             </div>
-            <!-- /.row -->
-
         </div>
-        <!-- /.container-fluid -->
-        </section>
-        <!-- /.section -->
-
     </div>
-    <!-- /.main-page -->
+
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+      <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+  <script src="js/pace/pace.min.js"></script>
+  <script src="js/lobipanel/lobipanel.min.js"></script>
+  <script src="js/iscroll/iscroll.js"></script>
+  <script src="js/prism/prism.js"></script>
+  <script src="js/DataTables/datatables.min.js"></script>
+  <script src="js/main.js"></script>
 
 
-
-    </div>
-    <!-- /.content-container -->
-    </div>
-    <!-- /.content-wrapper -->
-
-    </div>
-    <!-- /.main-wrapper -->
-
-    <!-- ========== COMMON JS FILES ========== -->
     
-</body>
 
+    <script>
+    $(function($) {
+        $('#example').DataTable();
+
+        $('#example2').DataTable({
+            "scrollY": "300px",
+            "scrollCollapse": true,
+            "paging": false
+        });
+
+        $('#example3').DataTable();
+    });
+    </script>
+
+
+   
+</body>
 </html>
-<?php } ?>
