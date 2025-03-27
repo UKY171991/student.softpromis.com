d475735deda2cb3fcbc3352453672cd44b46068b
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-27 17:41:23 +0530

    Update add-candidate-to-batch.php

--- add-candidate-to-batch.php
+++ add-candidate-to-batch.php
@@ -137,7 +137,7 @@ if (strlen($_SESSION['alogin']) == "") {
                 <?php include('includes/leftbar.php'); ?>
 
                 <!-- Main Content -->
-                <main class="col-md-9 col-lg-10 px-md-4">
+                <main class="col-md-9 col-lg-10 px-md-4 mt-3">
                     <!-- Page Title -->
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
                         <h1 class="h2">Add Candidate to batch</h1>
