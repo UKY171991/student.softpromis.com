29c1c0dd80451fefd65283b50c59536750d01226
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-29 02:49:06 +0530

    result

--- add-result.php
+++ add-result.php
@@ -5,24 +5,35 @@ include('includes/config.php');
 if (strlen($_SESSION['alogin']) == "") {
     header("Location: index.php");
 } else {
-
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SOFTPRO | ADMIN </title>
-    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
+    <title>SOFTPRO | ADMIN</title>
+
+    <!-- <link rel="stylesheet" href="css/bootstrap.min.css" media="screen"> -->
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
     <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen">
     <link rel="stylesheet" href="css/prism/prism.css" media="screen">
-    <link rel="stylesheet" href="css/select2/select2.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css">
     <link rel="stylesheet" href="css/main.css" media="screen">
+    <link rel="stylesheet" href="css/mystyle.css"> 
     <script src="js/modernizr/modernizr.min.js"></script>
+
+  <!-- Bootstrap 5 CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Font Awesome 6 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <link rel="stylesheet" href="includes/style.css">
+
+
     <script>
     function getStudent(val) {
         $.ajax({
@@ -69,39 +80,50 @@ if (strlen($_SESSION['alogin']) == "") {
         });
     }
     </script>
+
+
+
+    
+    
+    <style>
+        .card { border: none; box-shadow: 0 2px 5px rgba(0,0,0,0.1); border-radius: 10px; }
+        .form-control:focus, .select2-container--default .select2-selection--single:focus { 
+            border-color: #007bff; 
+            box-shadow: 0 0 0 0.2rem rgba(0,123,255,0.25); 
+        }
+        .select2-container { width: 100% !important; }
+    </style>
 </head>
 
-<body class="top-navbar-fixed">
+<body class="bg-light">
     <div class="main-wrapper">
-        <!-- ========== TOP NAVBAR ========== -->
-        <?php include('includes/topbar.php'); ?>
-        <!-- ========== WRAPPER FOR BOTH SIDEBARS & MAIN CONTENT ========== -->
-        <div class="content-wrapper">
-            <div class="content-container">
-                <!-- ========== LEFT SIDEBAR ========== -->
+        <!-- Top Navbar -->
+        <?php include('includes/topbar-new.php'); ?>
+
+        <div class="container-fluid">
+            <div class="row">
+                <!-- Sidebar -->
+                <?php include('includes/left-sidebar-new.php'); ?>
                 <?php include('includes/leftbar.php'); ?>
-                <!-- /.left-sidebar -->
-                <div class="main-page">
-                    <div class="container-fluid">
-                        <div class="row page-title-div">
-                            <div class="col-md-6">
-                                <h2 class="title">Add result to batch</h2>
-                            </div>
-                            <!-- /.col-md-6 text-right -->
-                        </div>
-                        <!-- /.row -->
-                        <div class="row breadcrumb-div">
-                            <div class="col-md-6">
-                                <ul class="breadcrumb">
-                                    <li><a href="dashboard.php"><i class="fa fa-home"></i> Home</a></li>
-                                    <li class="active">Add result to batch</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <!-- /.row -->
+
+                <!-- Main Content -->
+                <main class="col-md-9 col-lg-10 px-md-4">
+                    <!-- Page Title -->
+                    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom mt-3">
+                        <h1 class="h2">Add Batch</h1>
                     </div>
-                    <div class="container-fluid">
-                        <div class="row">
+
+                    <!-- Breadcrumb -->
+                    <nav aria-label="breadcrumb" class="mb-4">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="dashboard.php"><i class="fas fa-home"></i> Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Add Batch</li>
+                        </ol>
+                    </nav>
+
+
+
+                    <div class="row">
                             <div class="col-md-12">
                                 <div class="panel">
                                     <div class="panel-body">
@@ -117,53 +139,66 @@ if (strlen($_SESSION['alogin']) == "") {
                                         </div>
                                         <?php } ?>
                                         <form class="form-horizontal" method="post">
-                                            <div class="form-group">
-                                                <label for="default" class="col-sm-2 control-label">Training
-                                                    Center</label>
-                                                <div class="col-sm-10">
-                                                    <select name="trainingcenterid" class="form-control clid"
-                                                        id="classid" onChange="getStudent(this.value);"
-                                                        required="required">
-                                                        <option value="">Select Training Center Name</option>
-                                                        <?php $sql = "SELECT * from tbltrainingcenter";
-                                                            $query = $dbh->prepare($sql);
-                                                            $query->execute();
-                                                            $trainings = $query->fetchAll(PDO::FETCH_OBJ);
-                                                            if ($query->rowCount() > 0) {
-                                                                foreach ($trainings as $training) {   ?>
-                                                        <option
-                                                            value="<?php echo htmlentities($training->TrainingcenterId); ?>">
-                                                            <?php echo htmlentities($training->trainingcentername); ?>&nbsp;
-                                                        </option>
-                                                        <?php }
-                                                            } ?>
-                                                    </select>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                
+                                                        <label for="default" class="control-label">Training
+                                                            Center</label>
+                                        
+                                                        <select name="trainingcenterid" class="form-control clid"
+                                                            id="classid" onChange="getStudent(this.value);"
+                                                            required="required">
+                                                            <option value="">Select Training Center Name</option>
+                                                            <?php $sql = "SELECT * from tbltrainingcenter";
+                                                                $query = $dbh->prepare($sql);
+                                                                $query->execute();
+                                                                $trainings = $query->fetchAll(PDO::FETCH_OBJ);
+                                                                if ($query->rowCount() > 0) {
+                                                                    foreach ($trainings as $training) {   ?>
+                                                            <option
+                                                                value="<?php echo htmlentities($training->TrainingcenterId); ?>">
+                                                                <?php echo htmlentities($training->trainingcentername); ?>&nbsp;
+                                                            </option>
+                                                            <?php }
+                                                                } ?>
+                                                        </select>
+                                                    
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="date" class="control-label ">Scheme Name</label>
+                                         
+                                                        <select name="schemeid" class="form-control stid" id="studentid"
+                                                            required="required" onChange="getsector(this.value);">
+                                                        </select>
+                                                        
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="default" class="control-label">Sector Name</label>
+                                                        
+                                                        <select name="sectorid" class="form-control stid" id="sectorid"
+                                                            required="required" onChange="getjobroll(this.value);">
+                                                        </select>
+                                                        
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="default" class="control-label">Jobroll Name</label>
+                                                       
+                                                        <select name="jobrollid" class="form-control stid" id="jobrollid"
+                                                            required="required" onChange="getbatch(this.value);">
+                                                        </select>
+                                                        
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="form-group">
-                                                <label for="date" class="col-sm-2 control-label ">Scheme Name</label>
-                                                <div class="col-sm-10">
-                                                    <select name="schemeid" class="form-control stid" id="studentid"
-                                                        required="required" onChange="getsector(this.value);">
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="default" class="col-sm-2 control-label">Sector Name</label>
-                                                <div class="col-sm-10">
-                                                    <select name="sectorid" class="form-control stid" id="sectorid"
-                                                        required="required" onChange="getjobroll(this.value);">
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="default" class="col-sm-2 control-label">Jobroll Name</label>
-                                                <div class="col-sm-10">
-                                                    <select name="jobrollid" class="form-control stid" id="jobrollid"
-                                                        required="required" onChange="getbatch(this.value);">
-                                                    </select>
-                                                </div>
-                                            </div>
+
+                                            
                                         </form>
                                     </div>
                                 </div>
@@ -217,6 +252,100 @@ if (strlen($_SESSION['alogin']) == "") {
                             </div>
                             <!-- /.col-md-6 -->
                         </div>
+
+
+                   
+
+                   
+                </main>
+            </div>
+        </div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+      <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+  <script src="js/pace/pace.min.js"></script>
+  <script src="js/lobipanel/lobipanel.min.js"></script>
+  <script src="js/iscroll/iscroll.js"></script>
+  <script src="js/prism/prism.js"></script>
+  <script src="js/main.js"></script>
+
+
+    <script>
+    $(function($) {
+        $(".js-states").select2();
+        $(".js-states-limit").select2({
+            maximumSelectionLength: 2
+        });
+        $(".js-states-hide").select2({
+            minimumResultsForSearch: Infinity
+        });
+    });
+    </script>
+
+
+   
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SOFTPRO | ADMIN </title>
+    
+</head>
+
+<body class="top-navbar-fixed">
+    <div class="main-wrapper">
+        <!-- ========== TOP NAVBAR ========== -->
+        <?php include('includes/topbar.php'); ?>
+        <!-- ========== WRAPPER FOR BOTH SIDEBARS & MAIN CONTENT ========== -->
+        <div class="content-wrapper">
+            <div class="content-container">
+                <!-- ========== LEFT SIDEBAR ========== -->
+                <?php include('includes/leftbar.php'); ?>
+                <!-- /.left-sidebar -->
+                <div class="main-page">
+                    <div class="container-fluid">
+                        <div class="row page-title-div">
+                            <div class="col-md-6">
+                                <h2 class="title">Add result to batch</h2>
+                            </div>
+                            <!-- /.col-md-6 text-right -->
+                        </div>
+                        <!-- /.row -->
+                        <div class="row breadcrumb-div">
+                            <div class="col-md-6">
+                                <ul class="breadcrumb">
+                                    <li><a href="dashboard.php"><i class="fa fa-home"></i> Home</a></li>
+                                    <li class="active">Add result to batch</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <div class="container-fluid">
+                        
                         <!-- /.col-md-12 -->
                     </div>
                 </div>
@@ -233,25 +362,7 @@ if (strlen($_SESSION['alogin']) == "") {
     <!-- /.content-wrapper -->
     </div>
     <!-- /.main-wrapper -->
-    <script src="js/jquery/jquery-2.2.4.min.js"></script>
-    <script src="js/bootstrap/bootstrap.min.js"></script>
-    <script src="js/pace/pace.min.js"></script>
-    <script src="js/lobipanel/lobipanel.min.js"></script>
-    <script src="js/iscroll/iscroll.js"></script>
-    <script src="js/prism/prism.js"></script>
-    <script src="js/select2/select2.min.js"></script>
-    <script src="js/main.js"></script>
-    <script>
-    $(function($) {
-        $(".js-states").select2();
-        $(".js-states-limit").select2({
-            maximumSelectionLength: 2
-        });
-        $(".js-states-hide").select2({
-            minimumResultsForSearch: Infinity
-        });
-    });
-    </script>
+    
 </body>
 
 </html>
