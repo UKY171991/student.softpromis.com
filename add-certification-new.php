86408e5e3fa77efc3f77f56f59ae3f661bb85d78
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-29 04:03:52 +0530

    Update add-certification-new.php

--- add-certification-new.php
+++ add-certification-new.php
@@ -32,7 +32,52 @@ if (strlen($_SESSION['alogin']) == "") {
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="includes/style.css">
 
+<script>
+    function getStudent(val) {
+        $.ajax({
+            type: "POST",
+            url: "get_student.php",
+            data: 'trainingid=' + val,
+            success: function(data) {
+                $("#studentid").html(data);
+            }
+        });
+    }
 
+    function getsector(val) {
+        $.ajax({
+            type: "POST",
+            url: "get_student.php",
+            data: 'schemeid=' + val,
+            success: function(data) {
+                $("#sectorid").html(data);
+            }
+        });
+    }
+
+    function getjobroll(val) {
+        $.ajax({
+            type: "POST",
+            url: "get_student.php",
+            data: 'sectorid=' + val,
+            success: function(data) {
+                $("#jobrollid").html(data);
+            }
+        });
+    }
+
+    function getbatch(val) {
+        $.ajax({
+            type: "POST",
+            url: "get_student.php",
+            data: 'jobrollid_for_certificate=' + val,
+            success: function(data) {
+                $("#batchid").html(data);
+
+            }
+        });
+    }
+    </script>
     
     
     <style>
@@ -80,13 +125,93 @@ if (strlen($_SESSION['alogin']) == "") {
                             <h5 class="mb-0">Add New Batch</h5>
                         </div>
                         <div class="card-body">
-                            <form method="post">
-                                
-
-                                <button type="submit" name="submit" class="btn btn-primary">
-                                    <i class="fas fa-check me-2"></i>Add Batch
-                                </button>
+                            <form class="form-horizontal" method="post">
+                                <div class="form-group">
+                                    <label for="default" class="col-sm-2 control-label">Training
+                                        Center</label>
+                                    <div class="col-sm-10">
+                                        <select name="trainingcenterid" class="form-control clid"
+                                            id="classid" onChange="getStudent(this.value);"
+                                            required="required">
+                                            <option value="">Select Training Center Name</option>
+                                            <?php $sql = "SELECT * from tbltrainingcenter";
+                                                $query = $dbh->prepare($sql);
+                                                $query->execute();
+                                                $trainings = $query->fetchAll(PDO::FETCH_OBJ);
+                                                if ($query->rowCount() > 0) {
+                                                    foreach ($trainings as $training) {   ?>
+                                            <option
+                                                value="<?php echo htmlentities($training->TrainingcenterId); ?>">
+                                                <?php echo htmlentities($training->trainingcentername); ?>&nbsp;
+                                            </option>
+                                            <?php }
+                                                } ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="date" class="col-sm-2 control-label ">Scheme Name</label>
+                                    <div class="col-sm-10">
+                                        <select name="schemeid" class="form-control stid" id="studentid"
+                                            required="required" onChange="getsector(this.value);">
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="default" class="col-sm-2 control-label">Sector Name</label>
+                                    <div class="col-sm-10">
+                                        <select name="sectorid" class="form-control stid" id="sectorid"
+                                            required="required" onChange="getjobroll(this.value);">
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="default" class="col-sm-2 control-label">Jobroll Name</label>
+                                    <div class="col-sm-10">
+                                        <select name="jobrollid" class="form-control stid" id="jobrollid"
+                                            required="required" onChange="getbatch(this.value);">
+                                        </select>
+                                    </div>
+                                </div>
                             </form>
+
+                            <table id="example" class="display table table-striped table-bordered"
+                                            cellspacing="0" width="100%">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Batch Name</th>
+                                        <th>Jobroll</th>
+                                        <th>Sector</th>
+                                        <th>Scheme</th>
+                                        <th>Training Center</th>
+                                        <th>Start Date</th>
+                                        <th>End Date</th>
+                                        <th>Start Time</th>
+                                        <th>End Time</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tfoot>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Batch Name</th>
+                                        <th>Jobroll</th>
+                                        <th>Sector</th>
+                                        <th>Scheme</th>
+                                        <th>Training Center</th>
+                                        <th>Start Date</th>
+                                        <th>End Date</th>
+                                        <th>Start Time</th>
+                                        <th>End Time</th>
+                                        <th></th>
+                                    </tr>
+                                </tfoot>
+                                <tbody id="batchid">
+
+                                </tbody>
+                            </table>
+                            
                         </div>
                     </div>
                 </main>
@@ -106,7 +231,17 @@ if (strlen($_SESSION['alogin']) == "") {
   <script src="js/prism/prism.js"></script>
   <script src="js/main.js"></script>
 
-
+ <script>
+    $(function($) {
+        $(".js-states").select2();
+        $(".js-states-limit").select2({
+            maximumSelectionLength: 2
+        });
+        $(".js-states-hide").select2({
+            minimumResultsForSearch: Infinity
+        });
+    });
+    </script>
     
 </body>
 </html>
@@ -139,52 +274,7 @@ if (strlen($_SESSION['alogin']) == "") {
     <link rel="stylesheet" href="css/select2/select2.min.css">
     <link rel="stylesheet" href="css/main.css" media="screen">
     <script src="js/modernizr/modernizr.min.js"></script>
-    <script>
-    function getStudent(val) {
-        $.ajax({
-            type: "POST",
-            url: "get_student.php",
-            data: 'trainingid=' + val,
-            success: function(data) {
-                $("#studentid").html(data);
-            }
-        });
-    }
-
-    function getsector(val) {
-        $.ajax({
-            type: "POST",
-            url: "get_student.php",
-            data: 'schemeid=' + val,
-            success: function(data) {
-                $("#sectorid").html(data);
-            }
-        });
-    }
-
-    function getjobroll(val) {
-        $.ajax({
-            type: "POST",
-            url: "get_student.php",
-            data: 'sectorid=' + val,
-            success: function(data) {
-                $("#jobrollid").html(data);
-            }
-        });
-    }
-
-    function getbatch(val) {
-        $.ajax({
-            type: "POST",
-            url: "get_student.php",
-            data: 'jobrollid_for_certificate=' + val,
-            success: function(data) {
-                $("#batchid").html(data);
-
-            }
-        });
-    }
-    </script>
+    
 </head>
 
 <body class="top-navbar-fixed">
@@ -232,55 +322,7 @@ if (strlen($_SESSION['alogin']) == "") {
                                             <?php echo htmlentities($error); ?>
                                         </div>
                                         <?php } ?>
-                                        <form class="form-horizontal" method="post">
-                                            <div class="form-group">
-                                                <label for="default" class="col-sm-2 control-label">Training
-                                                    Center</label>
-                                                <div class="col-sm-10">
-                                                    <select name="trainingcenterid" class="form-control clid"
-                                                        id="classid" onChange="getStudent(this.value);"
-                                                        required="required">
-                                                        <option value="">Select Training Center Name</option>
-                                                        <?php $sql = "SELECT * from tbltrainingcenter";
-                                                            $query = $dbh->prepare($sql);
-                                                            $query->execute();
-                                                            $trainings = $query->fetchAll(PDO::FETCH_OBJ);
-                                                            if ($query->rowCount() > 0) {
-                                                                foreach ($trainings as $training) {   ?>
-                                                        <option
-                                                            value="<?php echo htmlentities($training->TrainingcenterId); ?>">
-                                                            <?php echo htmlentities($training->trainingcentername); ?>&nbsp;
-                                                        </option>
-                                                        <?php }
-                                                            } ?>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="date" class="col-sm-2 control-label ">Scheme Name</label>
-                                                <div class="col-sm-10">
-                                                    <select name="schemeid" class="form-control stid" id="studentid"
-                                                        required="required" onChange="getsector(this.value);">
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="default" class="col-sm-2 control-label">Sector Name</label>
-                                                <div class="col-sm-10">
-                                                    <select name="sectorid" class="form-control stid" id="sectorid"
-                                                        required="required" onChange="getjobroll(this.value);">
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="default" class="col-sm-2 control-label">Jobroll Name</label>
-                                                <div class="col-sm-10">
-                                                    <select name="jobrollid" class="form-control stid" id="jobrollid"
-                                                        required="required" onChange="getbatch(this.value);">
-                                                    </select>
-                                                </div>
-                                            </div>
-                                        </form>
+                                        
                                     </div>
                                 </div>
                             </div>
@@ -290,42 +332,7 @@ if (strlen($_SESSION['alogin']) == "") {
                             <div class="col-md-12">
                                 <div class="panel">
                                     <div class="panel-body p-20">
-                                        <table id="example" class="display table table-striped table-bordered"
-                                            cellspacing="0" width="100%">
-                                            <thead>
-                                                <tr>
-                                                    <th>#</th>
-                                                    <th>Batch Name</th>
-                                                    <th>Jobroll</th>
-                                                    <th>Sector</th>
-                                                    <th>Scheme</th>
-                                                    <th>Training Center</th>
-                                                    <th>Start Date</th>
-                                                    <th>End Date</th>
-                                                    <th>Start Time</th>
-                                                    <th>End Time</th>
-                                                    <th></th>
-                                                </tr>
-                                            </thead>
-                                            <tfoot>
-                                                <tr>
-                                                    <th>#</th>
-                                                    <th>Batch Name</th>
-                                                    <th>Jobroll</th>
-                                                    <th>Sector</th>
-                                                    <th>Scheme</th>
-                                                    <th>Training Center</th>
-                                                    <th>Start Date</th>
-                                                    <th>End Date</th>
-                                                    <th>Start Time</th>
-                                                    <th>End Time</th>
-                                                    <th></th>
-                                                </tr>
-                                            </tfoot>
-                                            <tbody id="batchid">
-
-                                            </tbody>
-                                        </table>
+                                        
                                         <!-- /.col-md-12 -->
                                     </div>
                                 </div>
@@ -356,17 +363,7 @@ if (strlen($_SESSION['alogin']) == "") {
     <script src="js/prism/prism.js"></script>
     <script src="js/select2/select2.min.js"></script>
     <script src="js/main.js"></script>
-    <script>
-    $(function($) {
-        $(".js-states").select2();
-        $(".js-states-limit").select2({
-            maximumSelectionLength: 2
-        });
-        $(".js-states-hide").select2({
-            minimumResultsForSearch: Infinity
-        });
-    });
-    </script>
+   
 </body>
 
 </html>
