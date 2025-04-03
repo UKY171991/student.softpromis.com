82ce0b145e0f2017041212950b58e52877c0f18c
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-04-03 20:18:55 +0530

    Update left-sidebar-new.php

--- includes/left-sidebar-new.php
+++ includes/left-sidebar-new.php
@@ -52,11 +52,11 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                 </a>
                 <div class="collapse <?php if ($currentPage == 'create-trainingcenter.php' OR $currentPage == 'manage-trainingcenter.php' OR $currentPage == 'edit-trainingcenter.php') echo 'show'; ?>" id="trainingCenterSubmenu">
                     <ul class="nav flex-column ps-3">
-                        <li class="nav-item m-1">
-                            <a class="nav-link <?php if ($currentPage == 'create-trainingcenter.php') echo 'active'; ?>" href="create-trainingcenter.php"><i class="fa fa-plus me-2"></i>Create Training Center</a>
+                        <li class="nav-item">
+                            <a class="nav-link <?php if ($currentPage == 'create-trainingcenter.php') echo 'active m-1'; ?>" href="create-trainingcenter.php"><i class="fa fa-plus me-2"></i>Create Training Center</a>
                         </li>
-                        <li class="nav-item m-1">
-                            <a class="nav-link <?php if ($currentPage == 'manage-trainingcenter.php') echo 'active'; ?>" href="manage-trainingcenter.php"><i class="fa fa-cogs me-2"></i>Manage Training Center</a>
+                        <li class="nav-item">
+                            <a class="nav-link <?php if ($currentPage == 'manage-trainingcenter.php') echo 'active m-1'; ?>" href="manage-trainingcenter.php"><i class="fa fa-cogs me-2"></i>Manage Training Center</a>
                         </li>
                     </ul>
                 </div>
