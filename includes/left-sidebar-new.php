e7f521b466dd90c5d70893c3859fdff2f84541b6
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-04-03 21:14:34 +0530

    Update left-sidebar-new.php

--- includes/left-sidebar-new.php
+++ includes/left-sidebar-new.php
@@ -92,7 +92,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
         <div class="collapse <?php if ($currentPage == 'assign-sector.php' OR $currentPage == 'manage-sector.php' OR $currentPage == 'create-sector.php') echo 'show'; ?>" id="sectorSubmenu">
             <ul class="nav flex-column ps-3">
                 <li class="nav-item">
-                    <a class="nav-link <?php if ($currentPage == 'candidate-bulk-upload.php') echo 'active mt-1 mb-1'; ?>" href="create-sector.php"><i class="fa fa-plus me-2"></i>Create Sector</a>
+                    <a class="nav-link <?php if ($currentPage == 'create-sector.php') echo 'active mt-1 mb-1'; ?>" href="create-sector.php"><i class="fa fa-plus me-2"></i>Create Sector</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link <?php if ($currentPage == 'manage-sector.php') echo 'active mt-1 mb-1'; ?>" href="manage-sector.php"><i class="fa fa-cogs me-2"></i>Manage Sector</a>
