a2bddfbecf1888d14b021351f3718019e229b4ef
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-04-08 10:33:58 +0530

    Update account.php

--- account.php
+++ account.php
@@ -41,6 +41,9 @@ if (strlen($_SESSION['alogin']) == "") {
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="includes/style.css">
 
+  <!-- Include DataTables CSS -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+
 <style>
   body {
     background-color: #e9ecef;
