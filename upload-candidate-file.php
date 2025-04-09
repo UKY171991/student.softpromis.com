5cd94675900f6ce76d4b0beb24e8158c280e41bf
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-04-09 15:33:38 +0530

    Update upload-candidate-file.php

--- upload-candidate-file.php
+++ upload-candidate-file.php
@@ -18,10 +18,6 @@ if (strlen($_SESSION['alogin']) == "") {
         $query->execute();
         $result = $query->fetch(PDO::FETCH_OBJ);
 
-        // Initialize variables for SQL update
-        $updateFields = [];
-        $updateParams = [':cid' => $cid];
-
         // Check and process candidate photo
         if (!empty($_FILES['candidatephoto']['name'])) {
             if ($_FILES['candidatephoto']['size'] > $maxFileSize) {
@@ -33,12 +29,9 @@ if (strlen($_SESSION['alogin']) == "") {
                 }
                 $candidatephoto = ($_FILES['candidatephoto']['name']);
                 $candidatephototarget = 'doc/' . basename($candidatephoto);
-                if (move_uploaded_file($_FILES['candidatephoto']['tmp_name'], $candidatephototarget)) {
-                    $updateFields[] = "candidatephoto = :candidatephoto";
-                    $updateParams[':candidatephoto'] = $candidatephoto;
-                } else {
-                    $error .= "Failed to upload candidate photo.<br>";
-                }
+                move_uploaded_file($_FILES['candidatephoto']['tmp_name'], $candidatephototarget);
+                $updateFields[] = "candidatephoto = :candidatephoto";
+                $updateParams[':candidatephoto'] = $candidatephoto;
             }
         }
 
@@ -53,12 +46,9 @@ if (strlen($_SESSION['alogin']) == "") {
                 }
                 $aadhaarphoto = ($_FILES['aadhaarphoto']['name']);
                 $aadhaarphototarget = 'doc/' . basename($aadhaarphoto);
-                if (move_uploaded_file($_FILES['aadhaarphoto']['tmp_name'], $aadhaarphototarget)) {
-                    $updateFields[] = "aadhaarphoto = :aadhaarphoto";
-                    $updateParams[':aadhaarphoto'] = $aadhaarphoto;
-                } else {
-                    $error .= "Failed to upload Aadhaar photo.<br>";
-                }
+                move_uploaded_file($_FILES['aadhaarphoto']['tmp_name'], $aadhaarphototarget);
+                $updateFields[] = "aadhaarphoto = :aadhaarphoto";
+                $updateParams[':aadhaarphoto'] = $aadhaarphoto;
             }
         }
 
@@ -73,12 +63,9 @@ if (strlen($_SESSION['alogin']) == "") {
                 }
                 $qualificationphoto = ($_FILES['qualificationphoto']['name']);
                 $qualificationphototarget = 'doc/' . basename($qualificationphoto);
-                if (move_uploaded_file($_FILES['qualificationphoto']['tmp_name'], $qualificationphototarget)) {
-                    $updateFields[] = "qualificationphoto = :qualificationphoto";
-                    $updateParams[':qualificationphoto'] = $qualificationphoto;
-                } else {
-                    $error .= "Failed to upload qualification photo.<br>";
-                }
+                move_uploaded_file($_FILES['qualificationphoto']['tmp_name'], $qualificationphototarget);
+                $updateFields[] = "qualificationphoto = :qualificationphoto";
+                $updateParams[':qualificationphoto'] = $qualificationphoto;
             }
         }
 
@@ -93,12 +80,9 @@ if (strlen($_SESSION['alogin']) == "") {
                 }
                 $applicationphoto = ($_FILES['applicationphoto']['name']);
                 $applicationphototarget = 'doc/' . basename($applicationphoto);
-                if (move_uploaded_file($_FILES['applicationphoto']['tmp_name'], $applicationphototarget)) {
-                    $updateFields[] = "applicationphoto = :applicationphoto";
-                    $updateParams[':applicationphoto'] = $applicationphoto;
-                } else {
-                    $error .= "Failed to upload application photo.<br>";
-                }
+                move_uploaded_file($_FILES['applicationphoto']['tmp_name'], $applicationphototarget);
+                $updateFields[] = "applicationphoto = :applicationphoto";
+                $updateParams[':applicationphoto'] = $applicationphoto;
             }
         }
 
